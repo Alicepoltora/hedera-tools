cc657031edb585aedcd2e7a613f05c0eddd0bae9
Alice <111580170+Alicepoltora@users.noreply.github.com>
2026-03-21 20:10:45 +0700

    build: add empty shim for optional hedera-wallet-connect deps

---
+++ src/shims/empty.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+// Empty shim for optional deps not used in the demo
+const noop = (): object => ({});
+const noopClass = class {};
+export default noop;
+export const createAppKit = noop;
+export const AppKit = noopClass;
+export const WalletKit = noopClass;
+export const createWalletKit = noop;
+export const WagmiAdapter = noopClass;
+export const SolanaAdapter = noopClass;
+export const EthersAdapter = noopClass;
+export const Ethers5Adapter = noopClass;
+export const Core = noopClass;
+export const create = noop;
+export const init = noop;
